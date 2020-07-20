f8b4dd678299b3acff148e6dad57700315b67871
juniorstar-1976 <57063017+juniorstar-1976@users.noreply.github.com>
2020-07-20 19:54:47 +0200

    Initial commit

---
+++ function.c
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+
+Main()
+{
+Function1()
+{ 
+    //initial code
+}
+=======
+=======
+>>>>>>> Develop2
+Main() 
+{ 
+Function1() 
+{ 
+//Initial Code 
+} 
+<<<<<<< HEAD
+Function2() 
+{ 
+//Developer A Code 
+} 
+>>>>>>> Develop
+=======
+Function3() 
+{ 
+//Developer B Code 
+} 
+>>>>>>> Develop2
+
+}
+
---
+++ function_REMOTE_3707.c
@@ -0,0 +1,14 @@
+Main() 
+{ 
+Function1() 
+{ 
+//Initial Code 
+} 
+Function2() 
+{ 
+//Developer A Code 
+} 
+
+}
+
+i am adding my git Hub comit file 
